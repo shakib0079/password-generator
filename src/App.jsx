b421d6109f1e28714fdbbb8e0ba815a878520f3d
shakib0079 <shakib100630@gmail.com>
2024-12-02 20:38:21 +0600

    Feat: Developed Password Genrator Apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react"
 
 function App() {
 
-  const [password, setPassword] = useState();
+  const [password, setPassword] = useState('');
   const [length, setLength] = useState(8);
-  // const [isNumberAllowed, setIsNumberAllowed] = useState(false);
-  // const [isCharAllowed, setIsCharAllowed] = useState(false);
+  const [isNumberAllowed, setIsNumberAllowed] = useState(false);
+  const [isCharAllowed, setIsCharAllowed] = useState(false);
 
-  const lengthHandler = () => {
-    setLength()
+
+  // const passwordGenerator = useCallback(() => {
+  //   let pass = '';
+  //   let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  //   if(isNumberAllowed) str += "1234567890"
+  //   if(isCharAllowed) str += `!@#$%^&*(:;.<>?/)`
+
+  //   for(let i = 1; i<= length; i++){
+  //     pass += str[Math.floor(Math.random() * str.length)]
+  //   }
+
+  //   setPassword(pass)
+  // }, [length, isNumberAllowed, isCharAllowed, setPassword])
+  const inputField = useRef(null)
+
+  const passwordCopyOperation = () => {
+    inputField.current?.select();
+    window.navigator.clipboard.writeText(password);
   }
-
+  
   useEffect(() => {
     let pass = '';
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(isNumberAllowed) str += "1234567890"
+    if(isCharAllowed) str += `!@#$%^&*(:;.<>?/)`
+
     for(let i = 1; i<= length; i++){
-      pass += str[Math.floor(Math.random() * str.length + 1)]
+      pass += str[Math.floor(Math.random() * str.length)]
     }
 
     setPassword(pass)
-  }, [length]);
+  }, [length, isNumberAllowed, isCharAllowed]);
   
 
   return (
@@ -28,20 +47,20 @@ function App() {
       <main className="flex flex-col items-center justify-center space-y-4 p-[40px] border bg-slate-900 text-white w-[80%]">
       <h1>Password Generator</h1>
       <div className="flex gap-2 w-[70%]">
-        <input type="text" placeholder="Password" value={password} className="w-[100%] border rounded-lg px-4 py-2 text-orange-600" />
-        <button className="bg-blue-500 px-4 py-2 rounded-lg">Copy</button>
+        <input ref={inputField} type="text" placeholder="Password" value={password} className="w-[100%] border rounded-lg px-4 py-2 text-orange-600" readOnly />
+        <button className="bg-blue-500 px-4 py-2 rounded-lg" onClick={passwordCopyOperation}>Copy</button>
       </div>
       <div className="flex w-[70%] items-center justify-start gap-4 py-4">
         <div className="flex items-center gap-2">
-          <input type="range" name="length" value={length} id="" min="6" max="100" onChange={lengthHandler}/>
+          <input type="range" name="length" value={length} id="" min="6" max="100" onChange={(e) => setLength(e.target.value)}/>
           <label>Length: {length}</label>
         </div>
         <div className="flex items-center gap-2">
-          <input type="checkbox" name="numbers" />
+          <input type="checkbox" defaultChecked={isNumberAllowed} name="numbers" onChange={() => setIsNumberAllowed((prev) => !prev)}/>
           <label>Numbers</label>
         </div>
         <div className="flex items-center gap-2">
-          <input type="checkbox" name="numbers" id="" />
+          <input type="checkbox" defaultChecked={isCharAllowed} name="numbers" onChange={() => setIsCharAllowed((prev) => !prev)} />
           <label>Characters</label>
         </div>
       </div>
